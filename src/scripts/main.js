5c98a0a116b07b35e2e0440440a4e28da8acdda4
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2024-06-12 21:38:09 +0300

    updated code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,25 @@
-'use strict';
+"use strict";
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
+window.addEventListener("hashchange", () => {
+  if (window.location.hash === "#menu") {
+    document.body.classList.add("page__body--with-menu");
   } else {
-    document.body.classList.remove('page__body--with-menu');
+    document.body.classList.remove("page__body--with-menu");
   }
 });
 
-const form = document.getElementById('subscribe-to-the-digest__form');
+const form = document.getElementById("subscribe-to-the-digest__form");
 
-form.addEventListener('submit', function handleSubmin(event) {
+form.addEventListener("submit", function handleSubmin(event) {
   event.preventDefault();
 
   form.reset();
 });
 
-const btnClose = document.getElementById('btn-ok');
+const btnClose = document.getElementById("btn-ok");
 
-const message = document.getElementById('message-block');
-// const allButton = document.getElementById('button');
+const message = document.getElementById("message-block");
 
-btnClose.addEventListener('click', function() {
-  message.style.display = 'none';
+btnClose.addEventListener("click", function () {
+  message.style.display = "none";
 });
-
-// allButton.addEventListener('click', function() {
-//   message.style.opacity = '1';
-// })
